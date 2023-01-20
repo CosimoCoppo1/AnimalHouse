551d8c516497f7623fb12796e7964076dd66a3ef
francescamazz <mazzetti.francy01@gmail.com>
2023-01-20 22:42:14 +0100

    pagina chi-siamo

--- game/src/views/AboutView.vue
+++ game/src/views/AboutView.vue
@@ -4,7 +4,7 @@
     <div class="hero">
       <div class="hero__content">
         <p class="intro-text">parlano di noi</p>
-        <h1 class="big-text">La storia dietro Animal House</h1>
+        <h1 class="big-text">La storia di Animal House</h1>
       </div>
       <video autoplay muted loop class="hero__video">
         <source src="../assets/about/bg__hero.mp4" type="video/mp4" />
@@ -16,12 +16,12 @@
       <div class="col">
         <p>
           Questa è una storia di passione e innovazione.<br />
-          <b>Animal House</b> è una realtà piccola ma dinamica specializzata nel
-          campo del Pet care. Siamo guidato da due elementi: passione per il
+          <b>Animal House</b> è una realtà piccola ma dinamica, specializzata
+          nel campo del Pet care. Siamo guidati da due elementi: passione per il
           nostro lavoro e la volontà di mettere i clienti al centro, animali e
-          padroni. E ci impegniamo, giorno dopo giorno, per offrirti tutto
-          questo (e molto altro) a portata di click.<br />Se stai cercando
-          qualcosa di nuovo, sei nel <b>posto giusto</b>.
+          padroni. Ci impegniamo, giorno dopo giorno, per offrirti tutto questo
+          (e molto altro) a portata di click.<br />Se stai cercando qualcosa di
+          nuovo, sei nel <b>posto giusto</b>.
         </p>
       </div>
     </div>
@@ -55,11 +55,11 @@
         </h3>
         <p class="description__text">
           Qui potrai trovare i <b>migliori</b> prodotti per i tuoi animali, per
-          tutti i gusti e per tutte le occasioni. Lavoriamo a stretto contatto
-          con tutti i fornitori per garantire la migliore qualità presente sul
-          mercato e offrirti la crème de la crèeme.<br />Crediamo
-          nell'importanza della qualità e non solo quando di parla di prodotti,
-          ma anche quando si parla di assistenza ai nostri clienti!
+          tutti i gusti e occasioni. Lavoriamo a stretto contatto con i
+          fornitori per garantire la migliore qualità presente sul mercato e
+          offrirti la crème de la crème.<br />Crediamo nell'importanza della
+          qualità e non solo quando di parla di prodotti, ma anche quando si
+          parla di assistenza.
         </p>
       </div>
     </div>
@@ -69,11 +69,7 @@
       <div class="row">
         <div class="col-md-6" align="right">
           <div class="image__container">
-            <img
-              src="../assets/services/gallery1.png"
-              alt="..."
-              class="image"
-            />
+            <img src="../assets/about/gallery1.jpg" alt="..." class="image" />
             <router-link type="button" class="btn" :to="{ path: '/negozio' }"
               >negozio</router-link
             >
@@ -81,11 +77,7 @@
         </div>
         <div class="col-md-6" align="left">
           <div class="image__container">
-            <img
-              src="../assets/services/gallery2.png"
-              alt="..."
-              class="image"
-            />
+            <img src="../assets/about/gallery2.jpg" alt="..." class="image" />
             <router-link type="button" class="btn" :to="{ path: '/servizi' }"
               >servizi</router-link
             >
@@ -93,49 +85,101 @@
         </div>
       </div>
     </div>
-    <div class="grid">
+    <div class="grid mb-5">
       <div class="col">
         <p class="description__text">
-          Vuoi accedere al negozio per ottenere tutte le informazioni sui nostri
-          prodotti e <b>comprare</b> o prenotare una <b>visita immediata</b> per
-          il tuo amico animale? Passa all'area
-          <a href="#" class="link">VIP</a> di Animal House, ottenendo questo e
-          molto altro.
+          Vuoi accedere al negozio per <b>acquistare</b> tutti i nostri prodotti
+          e prenotare una <b>consulenza immediata</b> per il tuo animale?<br />Passa
+          all'area
+          <a href="#">
+            <button class="btn btn-danger" type="button">VIP</button>
+          </a>
+          di Animal House!
         </p>
+      </div>
+    </div>
+
+    <!-- team section -->
+    <div class="container-fluid">
+      <h3 class="description__title big-text">Come nasce Animal House?</h3>
+      <div class="row">
+        <div
+          class="col-lg-4"
+          v-for="(member, index) in this.members"
+          :key="index"
+        >
+          <div class="team-member">
+            <img
+              class="mx-auto rounded-circle"
+              :src="require(`@/assets/about/${member.img}`)"
+              alt="..."
+            />
+            <h4>{{ member.name }}</h4>
+            <p class="text-muted">{{ member.animal }}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="grid py-0">
+      <div class="col">
+        <p class="description__text mb-5 text-center">
+          Da tre amici ad una <b>comunità</b> in continua crescita!<br />
+          <i>(alla fine l'abbiamo presa la birra...)</i>
+        </p>
+      </div>
+    </div>
+
+    <!-- timeline section -->
+    <TimelineComponent />
+
+    <!-- description section -->
+    <div class="grid mt-5">
+      <div class="col">
+        <h3 class="description__title big-text">Le persone di Animal House</h3>
+        <p class="description__text">
+          Il nostro staff è composto da oltre 100 persone che lavorano come
+          <b>Pet care specialist</b> e tutti siamo determinati nel diffondere la
+          passione e la cura degli animali a coloro a cui veniamo in
+          contatto.<br />
+          Facendo di questa <b>dedizione</b> un lavoro, avanziamo sostenendo
+          questi valori nel team, convinti che il successo dell'azienda scorra
+          attraverso la valorizzazione e la realizzazione del contributo di
+          ciascuno.
+        </p>
+      </div>
+    </div>
+
+    <!-- keywords section -->
+    <div class="container-fluid">
+      <div class="row mt-3 keyword">
+        <div
+          class="col-md-4"
+          v-for="(keyword, index) in this.keywords"
+          :key="index"
+        >
+          <img :src="require(`@/assets/about/${keyword.img}`)" alt=".." />
+          <h4>{{ keyword.title }}</h4>
+        </div>
       </div>
     </div>
 
     <!-- description section -->
-    <div class="grid">
+    <div class="grid mt-5">
       <div class="col">
-        <h3 class="description__title big-text">Come nasce Animal House?</h3>
+        <h3 class="description__title big-text">Resta in contatto con noi!</h3>
         <p class="description__text">
-          Qui potrai trovare i <b>migliori</b> prodotti per i tuoi animali, per
-          tutti i gusti e per tutte le occasioni. Lavoriamo a stretto contatto
-          con tutti i fornitori per garantire la migliore qualità presente sul
-          mercato e offrirti la crème de la crème.<br />Crediamo nell'importanza
-          della qualità e non solo quando di parla di prodotti, ma anche quando
-          si parla di assistenza ai nostri clienti!
+          A volte sappiamo meglio di te quello di cui hai <b>bisogno</b> e di
+          cosa necessita il tuo amico animale.<br />Per avere informazioni sui
+          prodotti in negozio o indicazioni sui servizi contattaci passando alla
+          sezione dedicata.<br />
+          <router-link :to="{ path: '/contatti' }">
+            <button type="button" class="btn btn-danger mt-3">chiamaci</button>
+          </router-link>
         </p>
       </div>
     </div>
-    <TimelineComponent />
-    <br />
-    <br />
-    <br />
-    <br />
-    <br />
-    <br />
-    <br />
-    <br />
-    <br />
-    <br />
   </div>
 </template>
-
-<!--
-partiti da una birra tra amici e ora siamo qui
--->
 
 <script>
 import TimelineComponent from "../components/TimelineComponent.vue";
@@ -143,6 +187,28 @@ import TimelineComponent from "../components/TimelineComponent.vue";
 export default {
   name: "AboutView",
   components: { TimelineComponent },
+  data() {
+    return {
+      members: [
+        {
+          name: "Cosimo Coppolaro",
+          animal: "ha un pappagallo e un cane",
+          img: "team1.jpg",
+        },
+        {
+          name: "Francesca Mazzetti",
+          animal: "ha un cane e un gatto",
+          img: "team2.jpg",
+        },
+        { name: "Luca Orlandello", animal: "ha due cani", img: "team3.jpg" },
+      ],
+      keywords: [
+        { title: "Puntiamo sulla personalità di ognuno", img: "button1.png" },
+        { title: "Sviluppiamo le competenze individuali", img: "button2.png" },
+        { title: "Incoraggiamo le nuove idee", img: "button3.png" },
+      ],
+    };
+  },
 };
 </script>
 
@@ -186,7 +252,7 @@ export default {
 
 .hero__content h1,
 .hero__content p {
-  color: #ffa806;
+  color: #f84444;
 }
 
 .hero__video {
@@ -221,6 +287,7 @@ export default {
   width: 100%;
   align-items: center;
   margin-bottom: 80px;
+  background: url("../assets/curiosity/bg__poster.png") no-repeat center center;
 }
 
 .poster__img {
@@ -232,6 +299,7 @@ export default {
   object-fit: cover;
   width: 100%;
   height: 100%;
+  border-radius: 20px;
 }
 
 .poster__content {
@@ -242,19 +310,23 @@ export default {
 }
 
 .poster__content h3 {
-  color: #ffa806;
-  font-weight: bold;
-  font-size: 40px;
+  color: #f84444;
+  font-size: 44px;
+  font-weight: bolder;
 }
 
 @media (max-width: 768px) {
   .poster {
     flex-wrap: wrap;
-    height: auto;
+    height: 100%;
   }
 
-  .poster__content {
+  .poster__content p {
     text-align: justify;
+  }
+
+  .poster__content h3 {
+    font-size: 38px;
   }
 
   .poster__img,
@@ -265,7 +337,7 @@ export default {
 
 /* description section */
 .description__title {
-  color: #ffa806;
+  color: #f84444;
   text-align: center;
   margin-top: 30px;
 }
@@ -276,7 +348,7 @@ export default {
 }
 
 .link {
-  color: #ffa806;
+  color: #f84444;
 }
 
 @media (max-width: 768px) {
@@ -314,6 +386,39 @@ export default {
 }
 
 .image__container .btn:hover {
-  background-color: #ffa806;
+  background-color: #f84444;
+}
+
+/* team section */
+.team-member {
+  margin-bottom: 50px;
+  text-align: center;
+}
+.team-member img {
+  width: 225px;
+  height: 225px;
+  border: 7px solid #fff;
+}
+.team-member h4 {
+  margin-top: 25px;
+  margin-bottom: 0;
+  font-weight: bold;
+}
+.team-member p {
+  margin-top: 0;
+}
+
+/* keywords section */
+.keyword {
+  text-align: center;
+  padding: 0;
+}
+
+.keyword h4 {
+  font-size: 16px;
+  text-transform: uppercase;
+  font-weight: bold;
+  color: #f84444;
+  margin-top: 5px;
 }
 </style>
