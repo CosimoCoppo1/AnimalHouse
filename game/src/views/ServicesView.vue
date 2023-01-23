2143bbb5b6c51852ff2af673835279954d215c68
francescamazz <mazzetti.francy01@gmail.com>
2023-01-23 16:50:12 +0100

    completata pagina servizi

--- game/src/views/ServicesView.vue
+++ game/src/views/ServicesView.vue
@@ -31,7 +31,7 @@
           Per ogni dubbio contattaci agli indirizzi in descrizione forniti alla
           sezione
           <router-link :to="{ path: '/contatti' }">
-            <button class="btn btn-primary contatti" type="button">
+            <button class="btn btn-primary" type="button">
               contatti
             </button> </router-link
           >.
@@ -75,6 +75,14 @@
           avvaliamo inoltre della collaborazione di specialisti nelle varie
           discipline.
         </p>
+        <p>
+          Per conoscere i servizi specifici per sede passa alla sezione
+          <router-link :to="{ path: '/contatti' }">
+            <button type="button" class="btn btn-info">
+              contatti
+            </button> </router-link
+          >.
+        </p>
       </div>
     </div>
 
@@ -92,7 +100,6 @@
 
     <!-- alert section -->
     <div class="container alert alert-info" role="alert">
-      Per conoscere i servizi specifici per sede passa alla sezione contatti.<br />
       Prenota subito un appuntamento passando all'area
       <a href="#">VIP</a> di Animal House.
     </div>
@@ -117,21 +124,13 @@
         </p>
       </div>
     </div>
-    <!-- <div v-for="(service, index) in this.services" :key="index">
-      <p>{{ service.name }}</p>
-      <p>{{ service.description }}</p>
-    </div> -->
+
+    <!-- poster section -->
     <div
       class="poster container"
       v-for="(service, index) in this.services"
       :key="index"
     >
-      <div class="poster__content">
-        <h3 class="description__title">{{ service.name }}</h3>
-        <p>{{ this.serviceInformation[index].action }}</p>
-        <p>{{ service.description }}</p>
-        <a href="" class="button">Call to action</a>
-      </div>
       <div class="poster__img">
         <img
           :src="
@@ -141,28 +140,123 @@
         />
         <!-- <img :src="service.image" alt="..." /> -->
       </div>
+      <div class="poster__content">
+        <h3 class="poster__title">{{ service.name }}</h3>
+        <p>{{ this.serviceInformation[index].action }}</p>
+        <p class="option">
+          <i class="bi bi-caret-right-fill"></i>{{ service.description }}
+        </p>
+        <a href="#">
+          <button type="button" class="btn btn-info">
+            Prenota un colloquio adesso!
+          </button>
+        </a>
+      </div>
     </div>
 
-    <!-- description section -->
-    <div class="grid mt-4">
+    <div class="grid">
       <div class="col">
-        <h3 class="description__title">
-          Per conoscere i servizi più in dettaglio accedi all'area
-          <a href="#">VIP</a>.
-        </h3>
+        <p class="description__text">
+          Alcuni servizi di Animal House possono non essere al momento
+          <b>disponibili</b>.<br />Ti incoraggiamo a vedere nella lista che
+          segue i servizi prenotabili al momento per animale nelle nostre sedi!
+        </p>
+      </div>
+    </div>
+
+    <!-- accordion section -->
+    <div class="accordion container" id="accordionExample">
+      <div
+        class="accordion-item"
+        v-for="(pet, index) in this.pets"
+        :key="index"
+      >
+        <h4 class="accordion-header" :id="`heading${index}`">
+          <button
+            class="accordion-button collapsed"
+            type="button"
+            data-bs-toggle="collapse"
+            :data-bs-target="`#collapse${index}`"
+            aria-expanded="false"
+            :aria-controls="`collapse${index}`"
+            @click="this.getBookableServices(`${pet._id}`)"
+          >
+            <b>{{ pet.name }}</b
+            >, servizi prenotabili:
+          </button>
+        </h4>
+        <div
+          :id="`collapse${index}`"
+          class="accordion-collapse collapse"
+          :aria-labelledby="`heading${index}`"
+          data-bs-parent="#accordionExample"
+        >
+          <div
+            class="accordion-body"
+            v-for="(service, index) in this.servicesPet"
+            :key="index"
+          >
+            Potrai effettuare il servizio di
+            <b class="text-info">{{ service.service.name }}</b> presso la sede
+            in <b>{{ service.location.address }}</b
+            >, <b>{{ service.location.city }}</b
+            >. <br />Ancora <b>{{ service.reservation_left }}</b> date
+            prenotabili.<br />Affrettati!
+          </div>
+        </div>
       </div>
     </div>
 
     <!-- description section -->
-    <div class="grid mt-4" id="staff">
+    <div class="grid" id="staff">
       <div class="col">
         <h3 class="description__title big-text">Lo staff</h3>
         <p class="description__text">
           <b>Professionisti Pet care certificati</b>.<br />Altro grande
           vantaggio è quello di avere la sicurezza che tutti coloro che offrono
-          un servizio per animali all’interno di Animal House sono
+          un servizio per animali all'interno di Animal House sono
           professionisti del settore.<br />Ciò è possibile grazie ad un
           controllo <b>attento</b> ed accurato.
+        </p>
+        <p class="mt-2">
+          Ci basiamo sul lavoro di squadra!<br />Ecco i nostri dottori di
+          <b>spicco</b>!
+        </p>
+      </div>
+    </div>
+
+    <!-- bubble section -->
+    <div class="container-fluid">
+      <div class="row">
+        <div
+          class="col-lg-4"
+          v-for="(member, index) in this.members"
+          :key="index"
+        >
+          <div class="bubble-member">
+            <img
+              class="mx-auto rounded-circle"
+              :src="require(`@/assets/services/${member.img}`)"
+              alt="..."
+            />
+            <h4 class="member__name">{{ member.name }}</h4>
+            <p class="text-muted member__job">{{ member.job }}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <!-- description section -->
+    <div class="grid">
+      <div class="col">
+        <p>
+          Conosci meglio le menti e i cuori di Animal House passando per la
+          pagina
+          <router-link :to="{ path: '/chi-siamo' }">
+            <button type="button" class="btn btn-info">
+              Chi siamo
+            </button></router-link
+          >.
         </p>
       </div>
     </div>
@@ -200,32 +294,50 @@ export default {
         {
           img: "serv0.jpg",
           action:
-            "I servizi di toilettatura consistono in una lunga serie di processi fondamentali per il benessere dei nostri amici animali; nello specifico questi sono i servizi che puoi trovare su Animal House:",
+            "I servizi di toilettatura ad Animal House consistono in una lunga serie di processi fondamentali per il benessere dei vostri amici animali; nello specifico questi sono i servizi compresi in una seduta sotto appuntamento nelle nostre sedi:",
         },
         {
           img: "serv1.jpg",
           action:
-            "Offrire il massimo del benessere al tuo cane è una priorità, specialmente nelle situazioni in cui non puoi essere presente. Il Pet Sitter, oltre ad essere un vero amante degli animali, è una persona estremamente qualificata e professionale capace di entrare subito in sintonia con il tuo cane e pronta a risolvere qualsiasi eventuale problematica comportamentale.",
+            "Offrire il massimo del benessere al tuo cane è una priorità, specialmente nelle situazioni in cui non puoi essere presente. Il servizio di dog sitter ad Animal House conta di un personale che, oltre ad essere composto da veri amanti degli animali, è estremamente qualificato e professionale, capace di entrare subito in sintonia con il tuo cane e pronto a risolvere qualsiasi eventuale problematica comportamentale.",
         },
         {
           img: "serv2.jpg",
           action:
-            "Affidarsi ad uno Studio Veterinario e ad un medico competente e soprattutto bravo è un fattore determinante per il benessere del tuo animale, offrendogli una qualità di vita migliore. Cosa comprendono i servizi per il tuo cane:",
+            "Affidarsi ad uno studio veterinario e ad un medico competente, e soprattutto empatico, è un fattore determinante per il benessere del tuo animale, offrendogli una qualità di vita migliore. Il servizio ad Animal House comprende:",
         },
         {
           img: "serv3.jpg",
           action:
-            "La visita veterinaria è di solito un evento stressante per il gatto? Dopo la prima visita da Animal House siamo certi che il tuo gatto non accuserà più queste difficoltà. Vieni nei nostri centri per usufruire di:",
+            "La visita veterinaria è di solito un evento stressante per il tuo gatto? Dopo la prima visita da Animal House siamo certi che il tuo pet non accuserà più queste difficoltà, sapendo di essere in mani delicate e amiche. Vieni nei nostri centri per usufruire di:",
         },
         {
           img: "serv4.jpg",
           action:
-            "Non solo cani e gatti, ma anche i nostri amici acquatici hanno diritto ad una cura e attenzione altrettanto di alto livello. Ecco i servizi di Animal House per veterinaria pesci:",
+            "Non solo cani e gatti, ma anche i nostri amici acquatici hanno diritto ad una cura e una attenzione altrettanto di qualità. Ecco i servizi di Animal House per veterinaria pesci:",
+        },
+      ],
+      members: [
+        {
+          name: "Dott.ssa Rossella Terragni",
+          job: "Direttore sanitario",
+          img: "team0.png",
+        },
+        {
+          name: "Dott. Massimo Vignoli",
+          job: "Diagnostica per immagini",
+          img: "team1.jpg",
+        },
+        {
+          name: "Dott.ssa Valentina Pagliardini",
+          job: "Ecografia",
+          img: "team2.jpg",
         },
       ],
       locations: [],
       pets: [],
       services: [],
+      servicesPet: [],
       colors: [
         "#469374",
         "#9341b3",
@@ -252,6 +364,11 @@ export default {
         .get("http://localhost:8000/services")
         .then((response) => (this.services = response.data));
     },
+    getBookableServices(petId) {
+      axios
+        .get(`http://localhost:8000/bookable_services?pet=${petId}`)
+        .then((response) => (this.servicesPet = response.data));
+    },
     changeColor(index) {
       for (let i = 0; i <= 5; i++) {
         if (index % 6 == i) {
@@ -269,6 +386,12 @@ export default {
 </script>
 
 <style>
+.btn {
+  background-color: #94bbb8;
+  color: #fff;
+  border: 1px solid black;
+}
+
 /* hero section */
 .hero {
   height: 38vh;
@@ -282,12 +405,6 @@ export default {
 /* image gallery section */
 .image-gallery {
   background: url("../assets/curiosity/bg__poster.png") no-repeat center center;
-}
-
-.contatti {
-  background-color: #90e0ef;
-  border: 1px solid black;
-  color: #2c3e50;
 }
 
 .image__container .btn:hover {
@@ -330,19 +447,18 @@ export default {
   }
 }
 
-/* accordion section */
-@media (min-width: 768px) {
-  .accordion {
-    width: 50%;
-  }
-}
-
 /* poster section */
 .poster {
   display: flex;
   height: 80vh;
   width: 100%;
   align-items: center;
+}
+
+.poster__title {
+  font-size: 55px;
+  font-weight: bold;
+  color: #90e0ef;
 }
 
 .poster__img {
@@ -361,6 +477,10 @@ export default {
   padding: 50px;
 }
 
+.option {
+  font-weight: bold;
+}
+
 @media (max-width: 768px) {
   .poster {
     flex-wrap: wrap;
@@ -371,6 +491,34 @@ export default {
   .poster__content {
     width: 100%;
   }
+}
+
+/* accordion section */
+.accordion-button,
+.accordion-body {
+  font-size: 22px;
+}
+
+.accordion-button:hover {
+  color: #90e0ef;
+}
+
+@media (max-width: 768px) {
+  .accordion-button,
+  .accordion-body {
+    font-size: 17px;
+  }
+}
+
+/* bubble section */
+.member__name {
+  font-weight: bold;
+  font-size: 18px;
+  color: #90e0ef;
+}
+
+.member__job {
+  font-size: 15px;
 }
 </style>
 
